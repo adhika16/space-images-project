e481fc4937d543a8f949b712abe5b77388ca794d
Adhika P <40765693+adhika16@users.noreply.github.com>
2023-12-09 10:14:37 +0700

    Update App.jsx and styling
    - update App.jsx to render image album components
    - update index.css for display=block

--- reactapp/src/App.jsx
+++ reactapp/src/App.jsx
@@ -1,59 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { blueGrey } from '@mui/material/colors';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: blueGrey[500]
+    },
+  },
+});
 
 export default class App extends Component {
-    static displayName = App.name;
-
-    constructor(props) {
-        super(props);
-        this.state = { forecasts: [], loading: true };
-    }
-
-    componentDidMount() {
-        this.populateWeatherData();
-    }
-
-    static renderForecastsTable(forecasts) {
-        return (
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>Date</th>
-                        <th>Temp. (C)</th>
-                        <th>Temp. (F)</th>
-                        <th>Summary</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {forecasts.map(forecast =>
-                        <tr key={forecast.date}>
-                            <td>{forecast.date}</td>
-                            <td>{forecast.temperatureC}</td>
-                            <td>{forecast.temperatureF}</td>
-                            <td>{forecast.summary}</td>
-                        </tr>
-                    )}
-                </tbody>
-            </table>
-        );
-    }
-
-    render() {
-        let contents = this.state.loading
-            ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-            : App.renderForecastsTable(this.state.forecasts);
-
-        return (
-            <div>
-                <h1 id="tabelLabel" >Weather forecast</h1>
-                <p>This component demonstrates fetching data from the server.</p>
-                {contents}
-            </div>
-        );
-    }
-
-    async populateWeatherData() {
-        const response = await fetch(`${import.meta.env.VITE_REACT_APP_SPACEIMAGES_BASE_API}/weatherforecast`);
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
-    }
+  render() {
+    return (
+      <div>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Navbar />
+          <Home />
+          <Footer />
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
